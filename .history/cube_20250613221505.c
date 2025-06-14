6b1ca4efd301fcaaa36aeb11fb30a05f7042180d
tom00ti <t.s.izumi@gmail.com>
2025-06-14 18:46:32 +0900

    gitへ登録

---
+++ .history/cube_20250613221505.c
@@ -0,0 +1,11 @@
+#include <math.h>
+
+float A, B, C;
+
+float calculateX(int i, int j, int k){
+  return j*sin(A)*sin(B)*cos(C)-k*cos(A);
+}
+
+int main(){ return 0;}
+
+
---
+++ .history/cube_20250614183711.c
@@ -0,0 +1,32 @@
+/*** spinning cube ***/
+
+#include <math.h>
+#include <stdio.h>
+
+float A, B, C;
+
+
+float calculateX(int i, int j, int k) {
+  return j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B) * cos(C) +
+         j * cos(A) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * sin(C);
+}
+
+float calculateY(int i, int j, int k) {
+  return j * cos(A) * cos(C) + k * sin(A) * cos(C) -
+         j * sin(A) * sin(B) * sin(C) + k * cos(A) * sin(B) * sin(C) -
+         i * cos(B) * sin(C);
+}
+
+float calculateZ(int i, int j, int k) {
+  return k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B);
+}
+
+int main() {
+  printf("\x1b[2J");
+  while (1)
+  {
+    /* code */
+  }
+  
+  return 0;
+}
